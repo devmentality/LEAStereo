9537a00d4c09a2d15fdac47b269a9f8b2fc5d115
devmentality <nikita.volnov.1999@gmail.com>
2022-06-05 14:17:13 +0500

    fix args

--- config_utils/predict_args.py
+++ config_utils/predict_args.py
@@ -8,15 +8,19 @@ def obtain_predict_args():
     parser.add_argument('--maxdisp', type=int, default=192, help="max disp")
     parser.add_argument('--resume', type=str, default='', help="resume from saved model")
     parser.add_argument('--cuda', type=bool, default=False, help='use cuda?')
+    parser.add_argument('--data_path', type=str, required=True, help="data root")
+    parser.add_argument('--test_list', type=str, required=True, help="training list")
+    parser.add_argument('--save_path', type=str, default='./result/', help="location to save result")
+
+    # Dataset choices
     parser.add_argument('--sceneflow', type=int, default=0, help='sceneflow dataset? Default=False')
     parser.add_argument('--kitti2012', type=int, default=0, help='kitti 2012? Default=False')
     parser.add_argument('--kitti2015', type=int, default=0, help='kitti 2015? Default=False')
     parser.add_argument('--middlebury', type=int, default=0, help='Middlebury? Default=False')
     parser.add_argument('--satellite', type=int, default=0, help='Satellite? Default=False')
-    parser.add_argument('--data_path', type=str, required=True, help="data root")
-    parser.add_argument('--test_list', type=str, required=True, help="training list")
-    parser.add_argument('--save_path', type=str, default='./result/', help="location to save result")
-    ######### LEStereo params####################
+    parser.add_argument('--mvs3d', type=int, default=0, help='MVS3D? Default=False')
+
+    # LEStereo params
     parser.add_argument('--fea_num_layers', type=int, default=6)
     parser.add_argument('--mat_num_layers', type=int, default=12)
     parser.add_argument('--fea_filter_multiplier', type=int, default=8)
