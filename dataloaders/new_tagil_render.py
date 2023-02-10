f0b06da345fd6d0c6f7356ae0b6ac08318300a71
devmentality <nikita.volnov.1999@gmail.com>
2023-02-10 10:40:08 +0500

    change render dir

--- dataloaders/new_tagil_render.py
+++ dataloaders/new_tagil_render.py
@@ -5,15 +5,17 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument("--in_dir", type=str, help="dataset dir")
+parser.add_argument("--out_dir", type=str, help="dataset dir")
 
 
 def main(args):
+    os.mkdir(args.out_dir)
     dirs = [d for d in os.scandir(args.in_dir) if d.is_dir()]
     for d in dirs:
         im_path = os.path.join(d.path, 'img_L.tif')
         disp_path = os.path.join(d.path, 'disp_L.tif')
-        im_render_path = os.path.join(d.path, 'img_L_render.jpeg')
-        disp_render_path = os.path.join(d.path, 'disp_L_render.jpeg')
+        im_render_path = os.path.join(args.out_dir,  f'{d.name}_L.jpeg')
+        disp_render_path = os.path.join(args.out_dir, f'{d.name}_disp_L.jpeg')
 
         render(im_path, im_render_path)
         render(disp_path, disp_render_path)
