780a5cc9ebb867b5bf045c67eedc2989e0cb5b2a
devmentality <nikita.volnov.1999@gmail.com>
2023-03-31 10:22:01 +0500

    render new tagil better

--- dataloaders/new_tagil_render.py
+++ dataloaders/new_tagil_render.py
@@ -5,17 +5,28 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument("--in_dir", type=str, help="dataset dir")
+parser.add_argument("--list", type=str, help="list of files")
 parser.add_argument("--out_dir", type=str, help="dataset dir")
 
 
 def main(args):
     os.mkdir(args.out_dir)
     dirs = [d for d in os.scandir(args.in_dir) if d.is_dir()]
+
+    if args.list is not None:
+        sample_set = set()
+        with open(args.list) as list_file:
+            for line in list_file:
+                if len(line.rstrip()) > 0:
+                    sample_set.add(line.rstrip())
+
+        dirs = [d for d in dirs if d.name in sample_set]
+
     for d in dirs:
         im_path = os.path.join(d.path, 'img_L.tif')
         disp_path = os.path.join(d.path, 'disp_L.tif')
-        im_render_path = os.path.join(args.out_dir,  f'{d.name}_L.jpeg')
-        disp_render_path = os.path.join(args.out_dir, f'{d.name}_disp_L.jpeg')
+        im_render_path = os.path.join(args.out_dir,  f'{d.name}_render_L.jpeg')
+        disp_render_path = os.path.join(args.out_dir, f'{d.name}_render_disp_L.jpeg')
 
         render(im_path, im_render_path)
         render(disp_path, disp_render_path)
@@ -27,12 +38,12 @@ def render(fin, fout):
     arr = np.array(im)
     mx = np.nanmax(arr)
     mi = np.nanmin(arr)
-    print(fout)
-    print(mi)
-    print(mx)
-    print(arr)
+    nans = np.isnan(arr)
 
-    arr = np.floor(arr * 255 / (mx - mi)).astype(np.uint8)
+    arr = np.floor(arr * 200 / (mx - mi)).astype(np.uint8)
+    rgb_arr = np.moveaxis(np.array([arr, arr, arr]), [0], [2])
+    rgb_arr[nans] = np.array([255, 0, 0])
+
     oim = Image.fromarray(arr)
     oim.save(fout)
 
