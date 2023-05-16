1bfad87bd19f3d9515fe3981f1dfc7f6bd376965
devmentality <nikita.volnov.1999@gmail.com>
2023-05-16 16:13:28 +0500

    Clean clouds filter

--- dataloaders/clean_new_tagil.py
+++ dataloaders/clean_new_tagil.py
@@ -9,6 +9,7 @@ from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument("--dir", type=str, help="dataset dir")
 parser.add_argument("--dry_run", type=bool, default=True, help="do hiding of invalid")
+parser.add_argument("--skip_hidden", type=int, default=1, help="skip hidden sample? 0-no,1-yes")
 
 required = {
     'img_L.tif',
@@ -21,6 +22,9 @@ required = {
 
 REQUIRED_NO_OCC = 0.3
 REQUIRED_NON_ZERO = 0.8
+
+HIGH_TH = 500
+HIGH_MAX_FRAC = 0.15
 
 
 def is_img_valid(path: str) -> bool:
@@ -39,16 +43,30 @@ def is_disp_valid(path: str) -> bool:
     return no_occ_frac >= REQUIRED_NO_OCC
 
 
+def valid_high_frac(path: str) -> bool:
+    img = Image.open(path)
+    arr = np.array(img)
+    high_frac = np.count_nonzero(arr > HIGH_TH) / (arr.shape[0] * arr.shape[1])
+    return high_frac < HIGH_MAX_FRAC
+
+
 def main(args):
     dirs = [d for d in os.scandir(args.dir) if d.is_dir()]
     for i in range(len(dirs)):
         d = dirs[i]
+        if d.name.startswith('.') and args.skip_hidden:
+            print(f'skipping {d.name}')
+            continue
+
         is_valid = (
             is_img_valid(os.path.join(d, 'img_L.tif')) and
             is_img_valid(os.path.join(d, 'img_R.tif')) and
             is_disp_valid(os.path.join(d, 'disp_L_lidar.tif')) and
-            is_disp_valid(os.path.join(d, 'disp_R_lidar.tif'))
+            is_disp_valid(os.path.join(d, 'disp_R_lidar.tif')) and
+            valid_high_frac(os.path.join(d, 'img_L.tif')) and
+            valid_high_frac(os.path.join(d, 'img_R.tif'))
         )
+
         if not is_valid and not args.dry_run:
             save_name = d.name if d.name.startswith('.') else f'.{d.name}'
             print(f'invalid sample {d.name}. hiding dir')
