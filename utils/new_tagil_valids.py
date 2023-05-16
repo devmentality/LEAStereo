cc3ded3f481aff63e7f8063fc95390cfa4d5b0f5
devmentality <nikita.volnov.1999@gmail.com>
2023-05-16 18:16:27 +0500

    better valids

--- utils/new_tagil_valids.py
+++ utils/new_tagil_valids.py
@@ -9,28 +9,31 @@ def main(args):
     all_cnt = 0
     ok_cnt = 0
 
-    for d in dirs:
-        subdirs = [sd for sd in os.scandir(d) if sd.is_dir()]
-        for sd in subdirs:
-            res_name= os.path.join(sd, '60_midd_eval.log')
-            print(f'handling {sd.path}')
-            all_cnt += 1
+    with open('metrcis.txt', 'w') as metrics_file:
+        for d in dirs:
+            subdirs = [sd for sd in os.scandir(d) if sd.is_dir()]
+            for sd in subdirs:
+                res_name= os.path.join(sd, '60_midd_eval.log')
+                print(f'handling {sd.path}')
+                all_cnt += 1
 
-            if os.path.exists(res_name):
-                with open(res_name) as res_file:
-                    lines = res_file.readlines()
-                    if len(lines) >= 3:
-                        header = lines[1]
-                        if header.startswith('vis% d_err% o_err% t_err% mean_err'):
-                            metrics = list(map(float, lines[2].split()))
-                            ok_cnt += 1
-                            print(f'vis: {metrics[0]}; d_err: {metrics[1]}; o_err: {metrics[2]}, t_err: {metrics[3]} mean_err: {metrics[4]}')
+                if os.path.exists(res_name):
+                    with open(res_name) as res_file:
+                        lines = res_file.readlines()
+                        if len(lines) >= 3:
+                            header = lines[1]
+                            if header.startswith('vis% d_err% o_err% t_err% mean_err'):
+                                metrics = list(map(float, lines[2].split()))
+                                ok_cnt += 1
+                                print(f'vis: {metrics[0]}; d_err: {metrics[1]}; o_err: {metrics[2]}, t_err: {metrics[3]} mean_err: {metrics[4]}')
+                                sample_name = f'{d.name}_{sd.name}'
+                                metrics_file.write(f'{sample_name} {metrics[1]} {metrics[3]}\n')
+                            else:
+                                print(f'file not matched. lines: {lines}')
                         else:
                             print(f'file not matched. lines: {lines}')
-                    else:
-                        print(f'file not matched. lines: {lines}')
-            else:
-                print('results does not exist')
+                else:
+                    print('results does not exist')
     print(f'all: {all_cnt}, ok: {ok_cnt}')
 if __name__ == "__main__":
     args = parser.parse_args()
