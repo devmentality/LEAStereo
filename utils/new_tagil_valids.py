7b67a601ad0ab98f82190d7d93436e6e94a4942e
devmentality <nikita.volnov.1999@gmail.com>
2023-05-16 12:33:36 +0500

    better

--- utils/new_tagil_valids.py
+++ utils/new_tagil_valids.py
@@ -6,12 +6,15 @@ parser.add_argument("--in_dir", type=str, help="dataset dir")
 
 def main(args):
     dirs = [d for d in os.scandir(args.in_dir) if d.is_dir()]
+    all_cnt = 0
+    ok_cnt = 0
 
     for d in dirs:
         subdirs = [sd for sd in os.scandir(d) if sd.is_dir()]
         for sd in subdirs:
             res_name= os.path.join(sd, '60_midd_eval.log')
             print(f'handling {sd}')
+            all_cnt += 1
 
             if os.path.exists(res_name):
                 with open(res_name) as res_file:
@@ -20,6 +23,7 @@ def main(args):
                         header = lines[1]
                         if header.startswith('vis% d_err% o_err% t_err% mean_err'):
                             metrics = list(map(float, lines[2].split()))
+                            ok_cnt += 1
                             print(f'd_err: {metrics[1]}; mean_err; {metrics[3]}')
                         else:
                             print(f'file not matched. lines: {lines}')
@@ -27,7 +31,7 @@ def main(args):
                         print(f'file not matched. lines: {lines}')
             else:
                 print('results does not exist')
-
+    print(f'all: {all_cnt}, ok: {ok_cnt}')
 if __name__ == "__main__":
     args = parser.parse_args()
     main(args)
